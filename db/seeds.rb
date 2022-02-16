c2aab16ddc9f092838b367a81923943bf4c4e31c
Douglas Berkley <douglasmberkley@gmail.com>
2022-02-16 17:09:27 +0900

    added user stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the DB....'
+HauntedHouse.destroy_all
+
+puts 'Creating haunted houses...'
+
+puts "... created #{HauntedHouse.count} haunted houses."
