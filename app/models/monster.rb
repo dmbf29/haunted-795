118d613803aa69d7fefe609ecba8f29fc495be2d
Douglas Berkley <douglasmberkley@gmail.com>
2022-02-18 10:38:04 +0900

    added image upload'

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,4 +3,5 @@ class Monster < ApplicationRecord
   has_many :curses, dependent: :destroy  # medusa.curses
   has_many :powers, through: :curses     # medusa.powers
   validates :name, presence: true
+  has_one_attached :photo
 end
